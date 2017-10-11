97d460725638c414c7f11c0f67be82e8b0ee0731
Marcin Kotlicki <kotlickimarcin@gmail.com>
2017-10-11 17:23:54 +0200

    Added core classes. Startet work on core.cpp

---
+++ PAG/PAG/Core.cpp
@@ -0,0 +1,52 @@
+#include "Core.hpp"
+#include "Window.hpp"
+#include "Mesh.hpp"
+#include "Shader.hpp"
+#include "Texture.hpp"
+#include <exception>
+#include <stdexcept>
+
+void Core::run()
+{
+}
+
+Core::Core()
+{
+	if (!glfwInit())
+		throw std::exception("Cannot initialize GLFW");
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = new Window();
+
+	if (!gladLoadGL())
+		throw std::runtime_error("Cannot initialize GLAD");
+
+	mesh = new Mesh();
+	shader = new Shader();
+	texture = new Texture();
+
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+
+Core::~Core()
+{
+	if(window)
+	{
+		delete window;
+	}
+	if(mesh)
+	{
+		delete mesh;
+	}
+	if(texture)
+	{
+		delete texture;
+	}
+	if(shader)
+	{
+		delete shader;
+	}
+}
---
+++ PAG/PAG/Core.hpp
@@ -0,0 +1,30 @@
+#ifndef Core_hpp
+#define Core_hpp
+#define GLFW_INCLUDE_NONE
+
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
+
+#include <stdio.h>
+#include <glad/glad.h> //Do pobierania funkcji
+#include <GLFW/glfw3.h> //G³ówna pêtla i inicjalizacja
+
+class Window;
+class Mesh;
+class Shader;
+class Texture;
+
+class Core
+{
+private:
+	Window* window;
+	Mesh* mesh;
+	Shader* shader;
+	Texture* texture;
+public:
+	void run();
+	Core();
+	~Core();
+};
+
+#endif /* Core_hpp */
